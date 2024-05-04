f33e967f5064edf42798a35aa415fb02ec0f6cca
izida <myizida@gmail.com>
2024-05-04 15:16:12 +0300

    user&blogs little bit created

--- myblog/blogs/models.py
+++ myblog/blogs/models.py
@@ -1,8 +1,39 @@
 from django.db import models
 
+
 # Create your models here.
 class Post(models.Model):
-    pass
+    title = models.CharField(
+        'Зоголовок поста',
+        max_length=254,
+        blank=False,
+        )
+    text = models.TextField(
+        'Текст поста',
+        blank=False,
+    )
+    pub_date = models.DateField()
+    author = models.ForeignKey(
+        'user.User',
+        on_delete=models.CASCADE,
+    )
+    location = models.ForeignKey(
+        'Location',
+        on_delete=models.CASCADE,
+        blank=True,
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        blank=False,
+    )
+    is_published = models.BooleanField(
+        blank=False,
+        default=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
 
 
 class Category(models.Model):
--- myblog/user/models.py
+++ myblog/user/models.py
@@ -1,6 +1,29 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 # Create your models here.
-class User(AbstractBaseUser):
-    pass
+class User(AbstractUser):
+    sex = models.BooleanField(
+        'Гендер',
+        blank=True,
+        null=True
+        )
+    nickname = models.CharField(
+        'Никнейм',
+        unique=True,
+        max_length=20,
+        blank=True,
+        null=True
+        )
+    status = models.CharField(
+        'Статус',
+        max_length=30,
+        blank=True,
+        null=True
+    )
+    bio = models.TextField(
+        'О себе',
+        blank=True,
+        null=True,
+        )
